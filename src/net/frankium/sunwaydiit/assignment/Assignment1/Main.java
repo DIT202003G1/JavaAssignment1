5a916b89bd660d51175a03264d6e7352dd28c0e1
mclt0568 <tis7bfrankie@gmail.com>
2020-10-09 21:30:17 +0800

    init q1 java file

--- src/net/frankium/sunwaydiit/assignment/Assignment1/Main.java
+++ src/net/frankium/sunwaydiit/assignment/Assignment1/Main.java
@@ -2,6 +2,7 @@ package net.frankium.sunwaydiit.assignment.Assignment1;
 
 public class Main {
     public static void main(String[] args) {
-        
+        //This file is empty, Please select Question1.java for question 1 main class
+        System.out.println("This file is empty, Please select Question1.java for question 1 main class");
     }
 }
---
+++ src/net/frankium/sunwaydiit/assignment/Assignment1/Question1.java
@@ -0,0 +1,7 @@
+package net.frankium.sunwaydiit.assignment.Assignment1;
+
+public class Question1 {
+	public static void main(String[] args) {
+		System.out.println("Test");
+    }
+}
