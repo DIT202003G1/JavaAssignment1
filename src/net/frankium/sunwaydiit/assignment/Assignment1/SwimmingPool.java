cd26f65ff494280ca50bb6f85a0e5fbb1cd40e9e
mclt0568 <tis7bfrankie@gmail.com>
2020-10-15 11:38:04 +0800

    did formulae same as class work

--- src/net/frankium/sunwaydiit/assignment/Assignment1/SwimmingPool.java
+++ src/net/frankium/sunwaydiit/assignment/Assignment1/SwimmingPool.java
@@ -1,34 +1,59 @@
 package net.frankium.sunwaydiit.assignment.Assignment1;
 
-public class SwimmingPool {
-	private double length, width, depth, fillingRate, drainingRate;
-	private static double conversionRate = 7.48052;
-	public SwimmingPool(double[] dimensions, double fillingRate, double drainingRate){
+public class SwimmingPool{
+	private double length, width, depth, fillingRate, drainingRate, waterAmount;
+	private static final double conversionRate = 7.48052;
+	
+	//Constructor
+	public SwimmingPool(double[] dimensions, double fillingRate, double drainingRate, double waterAmount){
 		this.setDimensions(dimensions);
 		this.setFillingRate(fillingRate);
 		this.setDrainingRate(drainingRate);
+		this.setWaterAmount(waterAmount);
 	}
-	
-	//estimate amount of water in gallons
-	public double estimateWaterAmountToFill(){ return cubicFootToGallon(this.getVolume()); }
-	public double estimateWaterAmountToFill(double preFilledInGallon){ return cubicFootToGallon(this.getVolume()) - preFilledInGallon; }
-	
-	//estimate fill time
-	public double estimateFillTime(double preFilledInGallon){ return (this.estimateWaterAmountToFill(preFilledInGallon)/this.getFillingRate()); };
-	public double estimateFillTime(){ return this.estimateWaterAmountToFill()/this.getFillingRate(); };
-	
-	//addWater >:)
-	
 	//convertors
 	private static double cubicFootToGallon(double cubicFoot){ return cubicFoot * conversionRate; }
 	private static double gallonToCubicFoot(double gallon){ return gallon/conversionRate; }
+	
+	//get volume
+	public double getVolume(){ return length * width * depth; }
+	public double getEmptyVolume(){ return (length * width * depth) - gallonToCubicFoot(waterAmount); }
+	public double getFilledVolume(){ return gallonToCubicFoot(waterAmount); }
+	
+	// fill, drain water
+	public void fillWaterAtRate(double time, double rate){
+		waterAmount += ( time * rate);
+		if (waterAmount > cubicFootToGallon(getVolume()) ){
+			waterAmount = cubicFootToGallon(getVolume()) ;
+			System.out.println("Pool Overflow");
+		}
+	};
+	public void drainWaterAtRate(double time, double drainRate){
+		waterAmount -= (time * drainRate);
+		if (waterAmount < 0) {
+			waterAmount = 0;
+			return;
+		}
+	}
+	
+	public int timeToFillThePool(){return (int) (  ( cubicFootToGallon(getVolume()) - waterAmount) / fillingRate);}
+	public int timeToDrainThePool(){return(int)(waterAmount/drainingRate + 0.5);}
+	
+	//calculate how much water is needed to fill in the pool completely
+	public double waterNeededToFillPool(){
+		if (waterAmount < cubicFootToGallon( getVolume())){
+			return cubicFootToGallon( getVolume()) - waterAmount;
+		}return 0;
+	}
+	
 	//setters
-	private void setFillingRate(double rate) {this.fillingRate = rate;};
-	private void setDrainingRate(double rate) {this.drainingRate = rate;};
-	private void setDimensions(double[] dimensions) {this.length = dimensions[0];this.width = dimensions[1];this.depth = dimensions[2];};
+	public void setWaterAmount(double waterAmount) { this.waterAmount = waterAmount; }
+	public void setFillingRate(double rate) {this.fillingRate = rate;};
+	public void setDrainingRate(double rate) {this.drainingRate = rate;};
+	public void setDimensions(double[] dimensions) {this.length = dimensions[0];this.width = dimensions[1];this.depth = dimensions[2];};
 	//getters
-	private double[] getDimensions(){ return new double[]{this.length,this.width,this.depth};}
-	private double getFillingRate(){return this.fillingRate; }
-	private double getDrainingRate(){return this.drainingRate; }
-	private double getVolume(){ return length * width * depth; }
+	public double getWaterAmount() { return waterAmount; }
+	public double[] getDimensions(){ return new double[]{this.length,this.width,this.depth};}
+	public double getFillingRate(){return this.fillingRate; }
+	public double getDrainingRate(){return this.drainingRate; }
 }
